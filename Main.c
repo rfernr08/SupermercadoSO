ab45b20ff9a3d64803de2b00add574695034a55c
Raul <rfernr08@estudiantes.unileon.es>
2023-12-27 12:09:55 +0100

    menorId y cambiar metodos

--- Main.c
+++ Main.c
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <limits.h>
 
 #define MAX_CLIENTS 20
 #define MAX_CAJEROS 3
@@ -38,26 +39,43 @@ int randomizer(int max, int min){
 
 // La cola de clientes se puede implementar con un array con los pids y expulsar al de menor pid, o una COLA como estructura de datos.
 int main(int argc, char* argv){
-    if(argc==1){
-        printf("No has introducido ningún argumento. Debes introducir un número de asistentes mayor que 1.\n");
+    if(argc==1 || numClientes < 1){
+        printf("Argumento introduccido incorrecto. Debes introducir un número de asistentes y que se mayor de 1.\n");
         return 1;
     }
+    pthread_mutex_init(&logSemaforo, NULL);
+    pthread_mutex_init(&repSemaforo, NULL);
+    pthread_mutex_init(&cliSemaforo, NULL);
+
+    pthread_cond_t repCondicion;
+    pthread_cond_init(&repCondicion, NULL);
+
+    logFile = fopen("logFile.txt", "w");
+    if (logFile == NULL) {
+        printf("Error opening log file.\n");
+        return 1;
+    }
+
+    int i;
+    for (i = 0; i < MAX_CLIENTS; i++) {
+        clientes[i] = 0;
+    }
+    
     struct sigaction ss;
-    // if(numClientes<1){
-    //     printf("El valor introducido es incorrecto. Debes introducir un número de asistentes mayor que 1.\n");
-    //     return 1;
-    // }
     ss.sa_handler = añadirCliente;
     sigaction(SIGUSR1, &ss, NULL);
     pthread_t cajero1, cajero2, cajero3, reponedor;
     pthread_attr_t attr;
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED); 
-    pthread_create (&cajero1, &attr, Cajero, "Cajero 1 en su puesto");
-    pthread_create (&cajero2, &attr, Cajero, "Cajero 2 en su puesto");
-    pthread_create (&cajero3, &attr, Cajero, "Cajero 3 en su puesto");
-    pthread_create (&reponedor, &attr, Reponedor, "Reponedor listo para ser util");
+    pthread_create(&cajero1, &attr, Cajero, "Cajero 1 en su puesto");
+    pthread_create(&cajero2, &attr, Cajero, "Cajero 2 en su puesto");
+    pthread_create(&cajero3, &attr, Cajero, "Cajero 3 en su puesto");
+    pthread_create(&reponedor, &attr, Reponedor, "Reponedor listo para ser util");
     pause();
-
+    if(pthread_mutex_destroy(&logSemaforo) != 0) exit(-1);
+    if(pthread_mutex_destroy(&repSemaforo) != 0) exit(-1);
+    if(pthread_mutex_destroy(&cliSemaforo) != 0) exit(-1);
+    return 0;
 }
 
 
@@ -94,7 +112,12 @@ void añadirCliente(int sig){
 }
 
 int menorIDCliente(){
-
+    int menorId = INT_MAX;
+    for(i = 0; i < MAX_CLIENTS; i++){
+        if(clientes[i] < menorId && clientes[i] != 0){
+            menorId = clientes[i];
+        }
+    }
+    return menorId;
 }
-
 
